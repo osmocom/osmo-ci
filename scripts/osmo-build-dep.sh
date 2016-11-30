c4ce20a93e3b21cf033e813501f44414253a17cc
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-11-30 02:45:41 +0100

    cosmetic: osmo-build-dep.sh: print example on error
    I've been asked at least twice what the contents of the expected env
    vars should be, so log an example on error.
    
    Change-Id: I635752e6033c57bfce90d8b0732bc402bf3014c8

--- scripts/osmo-build-dep.sh
+++ scripts/osmo-build-dep.sh
@@ -7,25 +7,33 @@ cfg="$3"
 set -e
 
 set +x
+example="
+Example:
+  export deps=\$PWD/deps inst=\$PWD/inst
+  mkdir -p \$deps
+  mkdir -p \$inst
+  MAKE=make PARALLEL_MAKE=-j8 osmo-build-dep.sh libosmocore
+"
+
 echo
 echo
 echo
 echo " =============================== $project ==============================="
 echo
 if [ -z "$project" ]; then
-	echo "internal failure: \$project is empty"
+	echo "internal failure: \$project is empty$example"
 	exit 1
 fi
 if [ -z "$deps" ]; then
-	echo "internal failure: \$deps is empty"
+	echo "internal failure: \$deps is empty$example"
 	exit 1
 fi
 if [ -z "$inst" ]; then
-	echo "internal failure: \$inst is empty"
+	echo "internal failure: \$inst is empty$example"
 	exit 1
 fi
 if [ -z "$MAKE" ]; then
-	echo "internal failure: \$MAKE is empty"
+	echo "internal failure: \$MAKE is empty$example"
 	exit 1
 fi
 set -x
