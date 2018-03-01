a73b13b80ff4f2f09560b04267b38bc4693b8212
Alexander Couzens <lynxis@fe80.eu>
2018-03-01 11:11:04 +0100

    jenkins: add job osmo-gsm-tester_gerrit
    The job verifies the gerrit patchset.
    
    Change-Id: If661675528059bc8a95d39a6489a5a9381f52677

---
+++ jobs/osmo-gsm-tester_run-gerrit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e -x
+
+# On our hardware, we actually use the example configuration as-is.
+export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/example"
+
+# debug: provoke a failure
+#export OSMO_GSM_TESTER_OPTS="-s debug -t fail"
+
+unlink osmo-gsm-tester/example/resources.conf || true
+ln -s resources.conf.prod osmo-gsm-tester/example/resources.conf
+
+export OSMO_GSM_TESTER_OPTS="-s sms:sysmo -s aoip_sms:sysmo -s gprs:sysmo"
+./osmo-gsm-tester/contrib/jenkins-make-check-and-run.sh
