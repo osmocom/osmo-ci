acdde1617b7a12eff9bb124935299341474d9b3a
Harald Welte <laforge@gnumonks.org>
2018-04-06 09:31:39 +0200

    add docker-cleanup.sh script
    This script should be executed regularly on all build slaves that
    have docker in order to discard unused images/layers.  It would
    be a good idea to call "fstrim /" afterwards in order to get more
    SSD performance.  However, the latter requires root access, and hence
    cannot be called by the 'osmocom-build' user and thus jenkins.
    
    Maybe we should install it as a cron job or systemd periodic timer job?
    
    Related: OS#3144
    Change-Id: I688b952578507a9cc28fe682221b5c7e3a245519

---
+++ scripts/docker-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# delete all but the latest images
+IMAGES=`docker image ls | grep \^osmocom-build | grep -v latest  | awk -F ' ' '{print $1":"$2}'`
+for f in $IMAGES; do
+	docker image rm $f
+done
+
+docker image prune -f
