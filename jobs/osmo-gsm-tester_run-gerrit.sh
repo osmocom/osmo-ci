8768ad510a1ccb45c8b71a7a7c2daa3d4c4cb957
Pau Espin Pedrol <pespin@sysmocom.de>
2018-09-20 11:23:33 +0200

    jobs: osmo-gsm-tester_run-gerrit: Update suites selected for verification
    Recently in osmo-gsm-tester some suite names were changed to have better
    unified naming. As a result, some of the suites used here need to
    updated.
    
    Change-Id: Iafe865ab5f607fc8acfcb3f72ebe4b0ef0f1322a

--- jobs/osmo-gsm-tester_run-gerrit.sh
+++ jobs/osmo-gsm-tester_run-gerrit.sh
@@ -10,5 +10,5 @@ export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/example"
 unlink osmo-gsm-tester/example/resources.conf || true
 ln -s resources.conf.prod osmo-gsm-tester/example/resources.conf
 
-export OSMO_GSM_TESTER_OPTS="-s sms:sysmo -s aoip_sms:sysmo -s gprs:sysmo"
+export OSMO_GSM_TESTER_OPTS="-s nitb_sms:sysmo -s sms:sysmo -s gprs:sysmo"
 ./osmo-gsm-tester/contrib/jenkins-make-check-and-run.sh
