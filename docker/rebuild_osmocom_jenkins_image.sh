f8a085347a8b573e616183286fbaa9bea0a90ccb
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2019-02-19 03:41:25 +0000

    docker: Use the same Dockerfile for jessie and stretch
    The binaries built on the Jenkins to be used by the osmo-gsm-tester
    require libgnutls.so.30. This is provided by the libgnutls30 package
    which is not available on jessie (Debian 8) and a backport requires
    some effort.
    
    Re-use the existing Dockerfile for jessie and stretch (Debian 9) using
    an argument. Refactor the shell script to pass the right argument. Add
    the debian version into the tag name to make space for buster (Debian
    10).
    
    Change-Id: I44c8e49a548cb64defd2eba868ec4bd435de90ba

--- docker/rebuild_osmocom_jenkins_image.sh
+++ docker/rebuild_osmocom_jenkins_image.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
-# In case the debian apt archive has become out of sync, try a --no-cache build if it fails.
-docker build -t osmocom:amd64 -f Dockerfile_osmocom_jenkins.amd64 . \
- || docker build --no-cache -t osmocom:amd64 -f Dockerfile_osmocom_jenkins.amd64 .
+#!/bin/bash
+
+# Executes docker build with the given parameters and retry in case of error.
+function build_once() {
+	# In case the debian apt archive has become out of sync, try a
+	# --no-cache build if it fails.
+
+	# shellcheck disable=SC2068
+	docker build $@ -f Dockerfile_osmocom_jenkins.amd64 . \
+	|| docker build --no-cache $@ -f Dockerfile_osmocom_jenkins.amd64 .
+}
+
+# Builds a container with a debian version and tag name as parameter.
+function build_container() {
+	local tag_name=${1}
+	local debian_version=${2}
+
+	echo "Building for ${debian_version} and setting tag ${tag_name}"
+	build_once "-t" "${tag_name}" "--build-arg" DEBIAN_VERSION="${debian_version}"
+}
+
+# Create containers using jessie (Debian 8.0) and stretch (Debian 9.0) as base.
+build_container osmocom:amd64 jessie
+build_container osmocom:deb9_amd64 stretch
