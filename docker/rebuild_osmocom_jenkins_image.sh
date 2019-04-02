23d603a3af45771244c9aef9aa440e91cf4f7614
Oliver Smith <osmith@sysmocom.de>
2019-04-02 11:47:58 +0200

    docker/rebuild_osmocom_jenkins_image.sh: pull images
    Prevent building docker images on top of outdated Debian images, where
    the package download feeds have been disabled.
    
    Related: Change-Id I1076bbb7d77bdc99f5d60d641c09ce965fb9dfc6
             (docker-playground.git)
    Related: OS#3869
    Change-Id: Id840094aec51bf51d920aaa017a2f99fcb866f55

--- docker/rebuild_osmocom_jenkins_image.sh
+++ docker/rebuild_osmocom_jenkins_image.sh
@@ -15,6 +15,9 @@ function build_container() {
 	local tag_name=${1}
 	local debian_version=${2}
 
+	echo "Pulling ${debian_version} image"
+	docker pull "debian:${debian_version}"
+
 	echo "Building for ${debian_version} and setting tag ${tag_name}"
 	build_once "-t" "${tag_name}" "--build-arg" DEBIAN_VERSION="${debian_version}"
 }
