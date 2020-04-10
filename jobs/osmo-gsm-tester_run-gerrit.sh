f6dec4217e29bad75d01b4497ccae772974c5216
Pau Espin Pedrol <pespin@sysmocom.de>
2020-04-10 18:33:22 +0200

    jobs: osmo-gsm-tester: Update path to sysmocom's specific setup
    Since osmo-gsm-tester.git I364e0859c0851b9167907a4d948b659a0f6a19fb the
    example/ directory has been renamed to sysmocom/
    
    Change-Id: I4c7516d2eb9636d37391a81dc9d484c0c04d7191

--- jobs/osmo-gsm-tester_run-gerrit.sh
+++ jobs/osmo-gsm-tester_run-gerrit.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e -x
 
-# On our hardware, we actually use the example configuration as-is.
-export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/example"
+# On our hardware, we actually use the sysmocom configuration as-is.
+export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/sysmocom"
 
 # debug: provoke a failure
 #export OSMO_GSM_TESTER_OPTS="-s debug -t fail"
 
-unlink osmo-gsm-tester/example/resources.conf || true
-ln -s resources.conf.prod osmo-gsm-tester/example/resources.conf
+unlink osmo-gsm-tester/sysmocom/resources.conf || true
+ln -s resources.conf.prod osmo-gsm-tester/sysmocom/resources.conf
 
 export OSMO_GSM_TESTER_OPTS="-s nitb_sms:sysmo -s sms:sysmo -s gprs:sysmo"
 ./osmo-gsm-tester/contrib/jenkins-make-check-and-run.sh
--- jobs/osmo-gsm-tester_ttcn3.sh
+++ jobs/osmo-gsm-tester_ttcn3.sh
@@ -2,13 +2,13 @@
 set -e -x
 
 # On our hardware, we actually use the ttcn3 configuration as-is.
-export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/ttcn3"
+export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/sysmocom/ttcn3"
 
 # debug: provoke a failure
 #export OSMO_GSM_TESTER_OPTS="-s debug -t fail"
 
-unlink osmo-gsm-tester/ttcn3/resources.conf || true
-ln -s resources.conf.prod osmo-gsm-tester/ttcn3/resources.conf
+unlink osmo-gsm-tester/sysmocom/ttcn3/resources.conf || true
+ln -s resources.conf.prod osmo-gsm-tester/sysmocom/ttcn3/resources.conf
 
 PATH="$PWD/osmo-gsm-tester/src:$PATH" \
   ./osmo-gsm-tester/ttcn3/jenkins-run.sh
