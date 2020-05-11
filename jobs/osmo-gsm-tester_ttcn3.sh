71cc851e35d3260a5d08083053f01f2e037c5d04
Pau Espin Pedrol <pespin@sysmocom.de>
2020-05-11 18:50:08 +0200

    osmo-gsm-tester: Point config parameter to file instead of its dirname
    Since osmo-gsm-tester.git Change-Id Ieca65b71b543c44cfcec8e83efd0fe053c432e55,
    the -c parameter holds the config file and not the directory where it is contained.
    
    Change-Id: I9aca57c4dd3180367eb17ec92177b90a9c411a6a

--- jobs/osmo-gsm-tester_ttcn3.sh
+++ jobs/osmo-gsm-tester_ttcn3.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # On our hardware, we actually use the ttcn3 configuration as-is.
-export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/sysmocom/ttcn3"
+export OSMO_GSM_TESTER_CONF="$PWD/osmo-gsm-tester/sysmocom/ttcn3/main.conf"
 
 # debug: provoke a failure
 #export OSMO_GSM_TESTER_OPTS="-s debug -t fail"
