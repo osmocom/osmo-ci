d02c86663e71b724a04e6f3eae05888583bdd160
Oliver Smith <osmith@sysmocom.de>
2021-01-12 13:35:27 +0100

    contrib/jenkins.sh: clone osmo-gsm-manuals too
    Make osmo-gsm-manuals.git available as ~/osmo-gsm-manuals, so it can be
    used by osmo-trx and osmo-bts (the only two projects building manuals,
    which are not running in docker as of writing) instead of cloning the
    repository in their contrib/jenkins.sh.
    
    A similar change to clone osmo-gsm-manuals in the debian-stretch-jenkins
    image used by all other jobs that build manuals, is done in
    docker-playground 7e4c8c6f1f798b9b8e57af97131ce3759528e0de.
    
    osmo-ci's contrib/jenkins.sh is called by the update-osmo-ci-on-slaves
    job.
    
    Related: OS#4912
    Change-Id: I742fd929e39ca32d6034a30af75b6c8e5b47b233

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 set -e -x
 
-cd ~/osmo-ci || (cd ~/ && git clone git://git.osmocom.org/osmo-ci && cd ~/osmo-ci)
-git rev-parse HEAD
-git status
+# Clone repository to ~/, or update existing
+# $1: name of osmocom project
+clone_repo() {
+	cd ~/"$1" || (cd ~/ && git clone git://git.osmocom.org/"$1" && cd ~/"$1")
+	git rev-parse HEAD
+	git status
 
-git fetch && git checkout -f -B master origin/master
+	git fetch && git checkout -f -B master origin/master
 
-git rev-parse HEAD
-git status
+	git rev-parse HEAD
+	git status
+}
+
+clone_repo osmo-ci
+clone_repo osmo-gsm-manuals
 
 if [ `uname` = "Linux" ] && [ "x${OSMO_CI_NO_DOCKER}" != "x1" ]; then
 	scripts/osmo-ci-docker-rebuild.sh
