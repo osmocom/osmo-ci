osmocom/osmo-ci

commit 27ee885a689163cede1fc0c7f76584ccbb7abb41

repo-install-test: apt update before install wget
Fix errors caused by outdated package index:
  + apt install -y wget
  ...
  Err:2 http://security.debian.org/debian-security stretch/updates/main amd64 libssl1.1 amd64 1.1.0l-1~deb9u2
    404  Not Found [IP: 151.101.130.132 80]

Change-Id: I9d28967dc556e95eec2e3e3d7ff88533154acf3c

scripts/repo-install-test/run-inside-docker.sh
@@ -110,6 +110,7 @@ configure_osmocom_repo_debian() {
 
 	# Add repository key
 	if ! [ -e "$release_key" ]; then
+		apt-get update
 		apt install -y wget
 		wget -O "$release_key" "https://$obs_repo/Release.key"
 	fi

commit 52578dde20fd034b92cff7817f13d86cf463fe3a

repo-install-test: install repo after repo key
In previous patch 27ee885a689163cede1fc0c7f76584ccbb7abb41, I made sure
that "apt-get update" runs before trying to install wget to download the
repository key. But of course the OBS repository should not be present
before installing the repo key, or else it will fail:

E: The repository 'http://download.opensuse.org/.../Debian_10 ./ InRelease' is not signed.

Fixes: 27ee885 ("repo-install-test: apt update before install wget")
Change-Id: If79484f9ffe2a14ce6481b53867f5aee111aa11b

scripts/repo-install-test/run-inside-docker.sh
@@ -106,7 +106,6 @@ configure_osmocom_repo_debian() {
 	local release_key="/var/cache/apt/${proj}_Release.key"
 
 	echo "Configuring Osmocom repository"
-	echo "deb http://$obs_repo ./" > "/etc/apt/sources.list.d/$proj.list"
 
 	# Add repository key
 	if ! [ -e "$release_key" ]; then
@@ -116,6 +115,7 @@ configure_osmocom_repo_debian() {
 	fi
 	apt-key add "$release_key"
 
+	echo "deb http://$obs_repo ./" > "/etc/apt/sources.list.d/$proj.list"
 	apt-get update
 }
 