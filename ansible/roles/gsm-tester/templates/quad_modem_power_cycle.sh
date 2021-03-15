c427bd6cf4c1f64b9c1c301489096eb1cf6e3967
Pau Espin Pedrol <pespin@sysmocom.de>
2021-03-15 14:51:11 +0100

    ansible: ogt: uhubctl has new cmdline syntax to specify ports
    Change-Id: Ie57d85dc0a66e053247cd5bb92a4b2dea4eb4309

--- ansible/roles/gsm-tester/templates/quad_modem_power_cycle.sh
+++ ansible/roles/gsm-tester/templates/quad_modem_power_cycle.sh
@@ -3,12 +3,12 @@ set -ex
 
 locations="$(uhubctl -n 1d50:4002 | grep "Current status for hub" | awk '{print $5}')"
 for l in $locations; do
-	uhubctl -p 123456 -a 0 -n 1d50:4002 -l $l
+	uhubctl -p 1,2,3,4,5,6 -a 0 -n 1d50:4002 -l $l
 done
 # give a lot of time to discharge capacitors on the board
 sleep 20
 for l in $locations; do
-	uhubctl -p 123456 -a 1 -n 1d50:4002 -l $l
+	uhubctl -p 1,2,3,4,5,6 -a 1 -n 1d50:4002 -l $l
 done
 attempts=30
 while [ "x$(uhubctl | grep -e 05c6 -e 1199 -c)" != "x{{ gsm_modems }}" ]; do
