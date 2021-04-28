f8099e55046bd873cc6a0f1c521dc88517ec2dd9
Oliver Smith <osmith@sysmocom.de>
2021-04-28 15:40:56 +0200

    OBS: add osmocom-packages-docker.sh
    Add a simple helper script to run osmocom-*-packages.sh in docker to
    avoid installing dependencies on the host system.
    
    Related: SYS#5370
    Depends: docker-playground Ibb55ad18d2ccf4313f52fa3e3c10d4420c84dced
    Change-Id: Icc89e20950c2aaa67b209340d1d797b76fce32d2

---
+++ scripts/osmocom-packages-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -ex
+# Run osmocom-*-packages.sh in a docker container, so dependencies don't need
+# to be installed on the host machine. See osmocom-nightly-packages.sh and
+# osmocom-latest-packages.sh for supported environment variables.
+SCRIPTS="$(realpath "$(dirname "$0")")"
+IMAGE="debian10-obs-submit"
+OSCRC="${OSCRC:-.oscrc}"
+
+if ! [ -f "$OSCRC" ]; then
+	echo "ERROR: missing OSCRC (should point to OSC credentials file)"
+	exit 1
+fi
+
+. "$SCRIPTS/common.sh"
+docker_images_require "$IMAGE"
+
+case "$FEED" in
+nightly|next|latest)
+	SCRIPT="osmocom-$FEED-packages.sh"
+	;;
+*)
+	# "2021q1" etc, osmocom-latest.sh verifies and uses $FEED
+	SCRIPT="osmocom-latest-packages.sh"
+	;;
+esac
+
+docker run \
+	-e "FEED=$FEED" \
+	-e "PACKAGES=$PACKAGES" \
+	-e "PROJ=$PROJ" \
+	-v "$SCRIPTS:/scripts" \
+	-v "$(realpath "$OSCRC"):/home/user/.oscrc" \
+	"$USER/$IMAGE" \
+	sh -c "cd ~ && /scripts/$SCRIPT"
