osmocom/osmo-ci

commit bf84ba9f69a0a2125bfd4327519b0e6fa212d664

coverity/build_Osmocom.sh: run 'make check' in do_build()
This enables building and running tests (check_PROGRAMS), so that
Coverity will be able to find bugs in their code too.

Change-Id: I3f3149a61df96646798c72d0ad31c41b97e6b81f

coverity/build_Osmocom.sh
@@ -18,7 +18,7 @@ do_build() {
 		--disable-doxygen \
 		$*
 
-	make $PARALLEL_MAKE
+	make $PARALLEL_MAKE check
 	make install
 }
 

commit ae41942214ef43bbb6aeb257bad2985765035ad7

coverity/build_Osmocom.sh: don't do check for all
'make check' does not pass for asn1c, it fails with the following. I
looked into it and the cause is not obvious to me, so in order to make
the jenkins job pass again I've changed it back to just run 'make'
instead.

Fix for:
  ...
  fatal: making test-suite.log: failed to create ../tests/98-attribute-class-OK.asn1.log
  fatal: making test-suite.log: failed to create ../tests/99-class-sample-OK.asn1.trs
  fatal: making test-suite.log: failed to create ../tests/99-class-sample-OK.asn1.log
  Makefile:735: recipe for target 'test-suite.log' failed
  make[3]: *** [test-suite.log] Error 1
  make[3]: Leaving directory '/home/osmocom-build/osmo-ci/coverity/source-Osmocom/asn1c/libasn1fix'
  Makefile:841: recipe for target 'check-TESTS' failed

Disable 'make check' for libusrp too as it also didn't pass:

  Making check in usrp2
  make[4]: Entering directory '/tmp/coverity/source-Osmocom/libusrp/firmware/src/usrp2'
  test -f `basename 'eeprom_boot.a51'` || ln -s 'eeprom_boot.a51' .
  test -f ../common/`basename 'eeprom_boot.a51'` -o \
          \! -f `dirname 'eeprom_boot.a51'`/../common/`basename 'eeprom_boot.a51'` \
          || ln -s `dirname 'eeprom_boot.a51'`/../common/`basename 'eeprom_boot.a51'` ../common/`basename 'eeprom_boot.a51'`
  sdas8051 -plosgff `basename 'eeprom_boot.a51'`
  sdcc -mmcs51 --no-xinit-opt -I../../../firmware/include -I../../../firmware/src/usrp2 -I../../../firmware/src/common -I../../../firmware/src/common -DHAVE_USRP2 \
          -c -o eeprom_init.rel `test -f 'eeprom_init.c' || echo './'`eeprom_init.c
  test -f `basename '_startup.a51'` || ln -s '_startup.a51' .
  test -f ../common/`basename '_startup.a51'` -o \
          \! -f `dirname '_startup.a51'`/../common/`basename '_startup.a51'` \
          || ln -s `dirname '_startup.a51'`/../common/`basename '_startup.a51'` ../common/`basename '_startup.a51'`
  sdas8051 -plosgff `basename '_startup.a51'`
  make[4]: *** No rule to make target '../../lib/libfx2.lib', needed by 'eeprom_boot.ihx'.  Stop.

All other 'make check' calls do work as expected, the build script runs
through again with this patch.

Fixes: bf84ba9f ("coverity/build_Osmocom.sh: run 'make check' in do_build()")

coverity/build_Osmocom.sh
@@ -5,6 +5,7 @@ set -e -x
 base_dir="$PWD"
 src_dir="$base_dir/source-Osmocom"
 prefix="$base_dir/install-Osmocom"
+check="check"
 
 install -d "$prefix"
 
@@ -18,14 +19,27 @@ do_build() {
 		--disable-doxygen \
 		$*
 
-	make $PARALLEL_MAKE check
+	make $PARALLEL_MAKE $check
 	make install
+}
+
+do_build_no_check() {
+	check=""
+	do_build "$@"
+	check="check"
 }
 
 build_default() {
 	pushd $1
 	shift
 	do_build $*
+	popd
+}
+
+build_no_check() {
+	pushd $1
+	shift
+	do_build_no_check $*
 	popd
 }
 
@@ -69,10 +83,10 @@ cd "$src_dir"
 rm -rf "$prefix"
 
 build_layer1api
-build_default asn1c
+build_no_check asn1c
 build_default libosmocore
 build_libasn1c
-build_libusrp
+build_no_check libusrp
 build_default libosmo-abis
 build_default libosmo-netif
 build_default libosmo-sccp