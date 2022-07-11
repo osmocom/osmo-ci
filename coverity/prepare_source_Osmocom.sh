c42a20a8f1a608697f326141fe095c18e31a4872
Oliver Smith <osmith@sysmocom.de>
2022-07-11 15:23:45 +0200

    coverity: don't build tests for all
    The coverity jenkins job has been failing since we tried to build tests
    for all projects (and in an earlier attempt, build and run tests for all
    projects). Make the coverity job work again by skipping build of tests
    for asn1c. I looked into it earlier and the cause wasn't obvious to me,
    and it's currently not worth spending time on.
    
    Fix for:
    libtool:   error: cannot find the library '../../skeletons/libasn1cskeletons.la' or unhandled argument '../../skeletons/libasn1cskeletons.la'
    
    Change-Id: Iba0bfe0e753812058436eaf515391eb1f4b09951

--- coverity/prepare_source_Osmocom.sh
+++ coverity/prepare_source_Osmocom.sh
@@ -1,42 +1,46 @@
 #!/bin/sh
 BASEDIR=source-Osmocom
 
+PROJECTS="
+  libasn1c
+  libosmo-abis
+  libosmocore
+  libosmo-dsp
+  libosmo-netif
+  libosmo-sccp
+  libsmpp34
+  libusrp
+  osmo-bsc
+  osmo-msc
+  osmo-mgw
+  osmo-ggsn
+  osmo-gbproxy
+  osmo-sgsn
+  osmo-bts
+  osmo-gmr
+  osmo-iuh
+  osmo-pcu
+  osmo-sysmon
+  osmo-sip-connector
+  osmo-trx
+  osmo-hlr
+  osmocom-bb
+  osmo-smlc
+  osmo-cbc
+  simtrace2
+  osmo-hnodeb
+  osmo-hnbgw
+  osmo-bsc-nat
+"
+
+PROJECTS_DONT_BUILD_TEST="
+  asn1c
+"
+
 [ -d $BASEDIR ] || mkdir -p $BASEDIR
 cd $BASEDIR
 
-for proj in \
-  asn1c \
-  libasn1c \
-  libosmo-abis \
-  libosmocore \
-  libosmo-dsp \
-  libosmo-netif \
-  libosmo-sccp \
-  libsmpp34 \
-  libusrp \
-  osmo-bsc \
-  osmo-msc \
-  osmo-mgw \
-  osmo-ggsn \
-  osmo-gbproxy \
-  osmo-sgsn \
-  osmo-bts \
-  osmo-gmr \
-  osmo-iuh \
-  osmo-pcu \
-  osmo-sysmon \
-  osmo-sip-connector \
-  osmo-trx \
-  osmo-hlr \
-  osmocom-bb \
-  osmo-smlc \
-  osmo-cbc \
-  simtrace2 \
-  osmo-hnodeb \
-  osmo-hnbgw \
-  osmo-bsc-nat \
-  ; do
-
+for proj in $PROJECTS $PROJECTS_DONT_BUILD_TEST; do
 	if [ -d $proj ]; then
 		if [ -z "$SRC_SKIP_FETCH" ]; then
 			(cd $proj && git fetch && git checkout -f -B master origin/master)
@@ -47,9 +51,11 @@ for proj in \
 	else
 		git clone git://git.osmocom.org/$proj
 	fi
+done
 
-	# We want to compile tests, but not execute them.  Using 'noinst_PROGRAMS'
-	# instead of 'check_PROGRAMS' allows building test binaries during 'make all'.
+# We want to compile tests, but not execute them.  Using 'noinst_PROGRAMS'
+# instead of 'check_PROGRAMS' allows building test binaries during 'make all'.
+for proj in $PROJECTS; do
 	files="$(git -C $proj grep -l check_PROGRAMS)"
 	if [ -n "$files" ]; then
 		(cd $proj && sed -i "s/check_PROGRAMS/noinst_PROGRAMS/" $files)
