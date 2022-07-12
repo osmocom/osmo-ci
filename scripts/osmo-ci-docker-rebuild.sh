fa8a74dfde572a17a95d908c5bef40d01abc7ced
Oliver Smith <osmith@sysmocom.de>
2022-07-12 11:33:12 +0200

    docker-rebuild: build erlang img on x86_64 only
    Build debian-bullseye-erlang on x86_64 nodes only, as the image doesn't
    get used on nodes with other arches. The image is only used by
    gerrit-verifications jobs for osmo_dia2gsum and osmo_gsup, which are
    restriected to run on these nodes:
    https://jenkins.osmocom.org/jenkins/label/osmocom-gerrit-debian9/
    
    Remove the check and comments for not building for debian 10 x32, as
    that node has been replaced with a debian 11 one.
    
    Related: OS#5453
    Change-Id: I5636dfc9e06a44b096f9dc55528d677ea2dfc352

--- scripts/osmo-ci-docker-rebuild.sh
+++ scripts/osmo-ci-docker-rebuild.sh
@@ -5,14 +5,7 @@ cd "$(dirname "$0")/.."
 docker_images_require \
 	"debian-buster-jenkins"
 
-if [ "$(arch)" = "i686" ] && \
-		grep -q '^ID=debian' /etc/os-release && \
-		grep -q '^VERSION_ID="10"' /etc/os-release; then
-	# Attempting to run debian-bullseye (11) in docker on debian 10 x86
-	# doesn't work. Skip it here for gtp0-deb10build32 until we've moved it
-	# away from debian 10.
-	echo "Skipping build of debian-bullseye-erlang (OS#5453)"
-else
+if [ "$(arch)" = "x86_64" ]; then
 	docker_images_require \
 		"debian-bullseye-erlang"
 fi
