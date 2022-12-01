osmocom/osmo-ci

commit 9139e76baa306cf467f81c7792000df499ec714c

osmo-ci-docker-rebuild: don't always build osmo-gsm-tester
Currently there is only one jenkins node labeled as
osmo-gsm-tester-build. Build the osmo-gsm-tester container only there,
not on all x86_64 nodes.

The /var/tmp/osmo-gsm-tester/state directory is set up by
ansible/roles/gsm-tester/tasks/main.yml.

Related: https://jenkins.osmocom.org/jenkins/label/osmo-gsm-tester-build/
Change-Id: I9191d6d54d516ce75f3f576f34463dcaad22c85a

scripts/osmo-ci-docker-rebuild.sh
@@ -18,7 +18,7 @@ else
 		"debian-bullseye-erlang"
 fi
 
-if [ "$(arch)" = "x86_64" ]; then
+if [ -d "/var/tmp/osmo-gsm-tester/state" ]; then
 	docker_images_require \
 		"osmo-gsm-tester"
 fi

commit ce5875fd077e3c9e3fa1dffd441ec334f3a9f58a

jobs/osmo-gsm-tester virtual: fix docker img
Build the docker image right before using it, instead of building it in
another jenkins job update-osmo-ci-on-slaves via
osmo-ci-docker-rebuild.sh.

The logic in osmo-ci-docker-rebuild.sh was broken. I didn't realize at
the time that this image is only used for the virtual osmo-gsm-tester,
not the physical ones. But only the machines running the physical
osmo-gsm-tester have the /var/tmp/osmo-gsm-tester/state path. The
virtual osmo-gsm-tester isn't running on these machines but on generic
jenkins nodes.

Building the image right before using it makes sense for this job, as it
is the only user of the image. If it was already built from the same
Dockerfile, a cached version is used.

Fix for:
  Unable to find image 'osmocom-build/osmo-gsm-tester:latest' locally

Fixes: 9139e76b ("osmo-ci-docker-rebuild: don't always build osmo-gsm-tester")
Change-Id: Icad9459de1d3a3a4e65ecacf7f903433bb504cc9

jobs/osmo-gsm-tester-runner.yml
@@ -224,6 +224,16 @@
       - copy_artifact:
          repo: osmo-gsm-tester_build-osmocom-bb
       - shell: |
+            # Build the docker image
+            rm -rf docker-playground
+            git clone \
+              --depth=1 \
+              --branch="$DOCKER_PLAYGROUND_BRANCH" \
+              https://gerrit.osmocom.org/docker-playground \
+              docker-playground
+            git -C docker-playground log --oneline
+            make -C docker-playground/osmo-gsm-tester
+
             unlink osmo-gsm-tester/sysmocom/resources.conf || true
 
             ln -s resources.conf.virtual osmo-gsm-tester/sysmocom/resources.conf
@@ -260,3 +270,7 @@
           name: "OSMO_GSM_TESTER_BRANCH"
           default: "master"
           description: "Which branch/sha should be used for testing"
+      - string:
+          name: "DOCKER_PLAYGROUND_BRANCH"
+          default: "master"
+          description: "Which branch/sha should be used for building the osmo-gsm-tester docker container"

scripts/osmo-ci-docker-rebuild.sh
@@ -10,8 +10,3 @@ if [ "$(arch)" = "x86_64" ]; then
 		"debian-bullseye-erlang" \
 		"debian-bullseye-jenkins"
 fi
-
-if [ -d "/var/tmp/osmo-gsm-tester/state" ]; then
-	docker_images_require \
-		"osmo-gsm-tester"
-fi