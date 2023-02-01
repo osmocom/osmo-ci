7bd02257c9f198f709350aa0f04eec7f5df6e09c
Oliver Smith <osmith@sysmocom.de>
2023-02-01 09:17:32 +0100

    coverity/local_test: fix copy error
    coverity/common.sh is a symlink to ../scripts/common.sh in this git
    repository. The local_test.sh already adjusts the symlink so it points
    to the right place after copying related scripts to /tmp/coverity.
    However when running the script again, cp would fail with:
    
    cp: '/home/user/code/osmo-dev/src/osmo-ci/coverity/common.sh' and '/tmp/coverity/common.sh' are the same file
    
    Remove the symlink to avoid this error.
    
    Change-Id: I081871e6e06547a90879bc4aa08ac2b5c5ddaeef

--- coverity/local_test.sh
+++ coverity/local_test.sh
@@ -6,6 +6,7 @@ export SRC_SKIP_FETCH=1
 export SRC_CLEAN=1
 
 mkdir -p /tmp/coverity
+rm -f /tmp/coverity/common.sh
 cp "$SCRIPT_DIR"/* /tmp/coverity
 ln -sf "$SCRIPT_DIR/../scripts/common.sh" /tmp/coverity/common.sh
 
