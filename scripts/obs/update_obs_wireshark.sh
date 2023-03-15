e7eba153157e228c7e03ab2d58880f3d4489fbec
Oliver Smith <osmith@sysmocom.de>
2023-03-15 14:43:15 +0100

    jobs: add Osmocom_OBS_wireshark
    Add a nightly jenkins job that does the following:
    * Clone Wireshark from upstream
    * Merge several Osmocom branches on top
    * Build a source package
    * Submit it to the osmocom:wireshark OBS project
    
    Related: OS#2537
    Change-Id: Ifb49c5cb22a4de0da30a920e5450a27172b11d73

---
+++ scripts/obs/update_obs_wireshark.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e
+DIR="$(realpath "$(dirname "$0")")"
+PROJ="$1"
+
+BRANCHES="
+	osmith/deb-packaging
+	laforge/amr-2023
+	laforge/doc-fixes
+	osmocom/qcdiag
+	laforge/rspro
+"
+
+prepare_git_repo() {
+	cd "$DIR"
+	if ! [ -d _cache/wireshark ]; then
+		mkdir -p _cache
+		git -C _cache clone https://gitlab.com/wireshark/wireshark.git
+		git -C _cache/wireshark remote add osmocom https://gitea.osmocom.org/osmocom/wireshark
+	fi
+
+	cd _cache/wireshark
+	git fetch --all
+	git clean -fdx
+	git checkout -f -B osmocom/all-in-one origin/master
+
+	for b in $BRANCHES; do
+		git merge --no-edit "osmocom/$b"
+	done
+}
+
+update_obs_project() {
+	cd "$DIR"
+	./update_obs_project.py \
+		--apiurl obs.osmocom.org \
+		--docker \
+		--allow-unknown-package \
+		--git-skip-checkout \
+		--git-skip-fetch \
+		--version-append "~osmocom" \
+		"$PROJ" \
+		wireshark
+}
+
+set -x
+prepare_git_repo
+
+if [ -n "$PROJ" ]; then
+	update_obs_project
+fi
