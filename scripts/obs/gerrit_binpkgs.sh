b2e128511d5aeae1eb27a7465197012fac643da2
Oliver Smith <osmith@sysmocom.de>
2024-04-25 13:42:15 +0200

    OBS: move gerrit-binpkgs logic to separate script
    As I'm preparing slides for a OsmoDevCon talk, I'm realizing that it is
    quite cumbersome for users to build debian packages from Osmocom git
    repositories (the same way gerrit does it, to reproduce errors locally).
    
    Move the logic from the gerrit-binpkgs job to a separate script that can
    be run from an Osmocom git repository, will figure out the repository
    name from there and only needs the target distribution as argument.
    
    Usage example:
      $ git clone https://gitea.osmocom.org/osmocom/osmo-ci
      $ cd libosmocore
      $ ../osmo-ci/scripts/obs/gerrit_binpkgs.sh debian:12  # or debian:11, debian:10, almalinux:8
    
    Change-Id: I100d8dfc0c58bdafe7efb0fa4108031ce10398a5

---
+++ scripts/obs/gerrit_binpkgs.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+SCRIPTS_OBS_DIR="$(realpath "$(dirname "$0")")"
+
+DISTRO="$1"
+if [ -z "$DISTRO" ]; then
+	echo "usage: gerrit-binpkgs.sh DISTRO"
+	echo "examples:"
+	echo "  gerrit-binpkgs.sh debian:12"
+	echo "  gerrit-binpkgs.sh almalinux:8"
+	exit 1
+fi
+
+GIT_REPO_DIR="$(git rev-parse --show-toplevel 2>/dev/null || true)"
+if [ -z "$GIT_REPO_DIR" ]; then
+	echo "ERROR: run inside a git repository of an Osmocom project"
+	exit 1
+fi
+
+CACHE_DIR="$SCRIPTS_OBS_DIR/_cache"
+PROJECT_NAME="$(basename "$GIT_REPO_DIR")"
+
+# Copy the source dir into the cache dir. It will be mounted inside the docker
+# containers for building source and binary packages (so using a symlink does
+# not work). Use rsync so it is very fast.
+echo ":: Copying the source to the cache dir"
+mkdir -p "$CACHE_DIR"
+rsync -a --delete "$GIT_REPO_DIR" "$CACHE_DIR"
+
+echo ":: Building the source package"
+"$SCRIPTS_OBS_DIR"/build_srcpkg.py \
+	--docker \
+	--feed master \
+	--git-skip-fetch \
+	--git-skip-checkout \
+	"$PROJECT_NAME"
+
+echo ":: Building the binary packages"
+"$SCRIPTS_OBS_DIR"/build_binpkg.py \
+	--docker "$DISTRO" \
+	"$PROJECT_NAME"
