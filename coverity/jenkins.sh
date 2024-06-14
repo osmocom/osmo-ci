osmocom/osmo-ci

commit 6a2451a0ad3dd2f5e3179d64446d9d553a37016d

jobs/coverity: adjust mount path to new home dir
With the related docker-playground patch, the home dir for the build
user has actually been changed from /home/build to /build. This doesn't
matter for all other jobs it seems, but the coverity job here fails
because it tries to access $HOME/osmo-ci/coverity.

Adjust the mount path, so it works again as expected.

Fix for:
  ./jenkins.sh: line 37: /build/osmo-ci/coverity/get_token.sh: No such file or directory

Related: docker-playground Ief8837bd9f89f51e66857a453f7fc4645620159f
Change-Id: If0286e10d1644464e9408db1bbf18c24f4b8d5a6

jobs/coverity.yml
@@ -17,7 +17,7 @@
             -e PARALLEL_MAKE="$PARALLEL_MAKE" \
             -u build \
             -v "$PWD:/build" \
-            -v "$HOME/osmo-ci/coverity:/home/build/osmo-ci/coverity:ro" \
+            -v "$HOME/osmo-ci/coverity:/build/osmo-ci/coverity:ro" \
             -v "/opt/coverity:/opt/coverity:ro" \
             -w /build/coverity \
             "$USER/debian-bookworm-build" \

commit afae3d79b5656fa24a4eeb6208e8f2ca379967b4

jobs/coverity: fix clean up permission errors
Don't mount the host's osmo-ci/coverity dir in /build/osmo-ci/coverity.
The current working dir is mounted as /build, and because docker creates
the /build/osmo-ci directory structure as root, we end up with having
subdirectories created by root in our current working dir. These then
cause an error on the next run when jenkins tries to clean them up.

Fixes: 6a2451a0 ("jobs/coverity: adjust mount path to new home dir")
Change-Id: Ie0aa02d3821df6d0505748a7dd70ff67e4cf1405

coverity/jenkins.sh
@@ -7,7 +7,7 @@ readlink /opt/coverity/current
 
 export PATH=$PATH:/opt/coverity/current/bin
 
-base_dir="$HOME/osmo-ci/coverity"
+base_dir="/opt/osmo-ci/coverity"
 src_dir="$PWD/source-Osmocom"
 cov_dir="$src_dir/cov-int"
 

jobs/coverity.yml
@@ -17,7 +17,7 @@
             -e PARALLEL_MAKE="$PARALLEL_MAKE" \
             -u build \
             -v "$PWD:/build" \
-            -v "$HOME/osmo-ci/coverity:/build/osmo-ci/coverity:ro" \
+            -v "$HOME/osmo-ci/coverity:/opt/osmo-ci/coverity:ro" \
             -v "/opt/coverity:/opt/coverity:ro" \
             -w /build/coverity \
             "$USER/debian-bookworm-build" \