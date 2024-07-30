osmocom/osmo-ci

commit 9b36d2a8163d7b880c5d0f7ab888d91d6c4ac06b

OBS: fix up-to-date check for strongswan-epdg
Move the code to transform the version from the git tag to the version
that we can use in debian/changelog to a separate function. Use it not
only when generating the version for debian/changelog, but also when
checking if the current git tag is up-to-date.

Without this patch, it always considers strongswan-epdg outdated:
  strongswan-epdg: building source package (outdated: osmo-epdg-0.1.2 <=> 0.1.2.osmo.epdg in OBS)

Change-Id: I515695d52f5d37b256ef163875417745897e5900

scripts/obs/lib/debian.py
@@ -92,6 +92,14 @@ def get_last_version_from_changelog(project):
     return ret
 
 
+def transform_version(version):
+    if "osmo-epdg-" in version:
+        version = f"{version.replace('osmo-epdg-', '', 1)}-osmo-epdg"
+
+    version = version.replace("-", ".")
+    return version
+
+
 def changelog_add_entry_if_needed(project, version):
     """ Adjust the changelog if the version in the changelog is different from
         the given version. """
@@ -113,10 +121,7 @@ def changelog_add_entry_if_needed(project, version):
         pass
 
     # Debian versions must start with a digit
-    if "osmo-epdg-" in version:
-        version = f"{version.replace('osmo-epdg-', '', 1)}-osmo-epdg"
-
-    version = version.replace("-", ".")
+    version = transform_version(latest_version)
 
     if version_changelog == version:
         return

scripts/obs/update_obs_project.py
@@ -7,6 +7,7 @@ import sys
 import traceback
 import lib
 import lib.config
+import lib.debian
 import lib.docker
 import lib.git
 import lib.metapkg
@@ -97,6 +98,7 @@ def build_srcpkg_if_needed(pkgs_remote, package, is_meta_pkg):
                     branch_missing_ok=delete)
             else:
                 latest_version = lib.git.get_latest_tag_remote(package)
+                latest_version = lib.debian.transform_version(latest_version)
 
         if latest_version is None:
             if delete and os.path.basename(package) in pkgs_remote:

commit c090d6cc65aa213dcea162183ac631d82b4facd7

OBS: fix changelog_add_entry_if_needed
Fix a copy paste error, the variable is named version here, not
latest_version.

Fixes: 9b36d2a8 ("OBS: fix up-to-date check for strongswan-epdg")
Change-Id: Idff3733d902be6f84bd62813f6e704267538a07b

scripts/obs/lib/debian.py
@@ -121,7 +121,7 @@ def changelog_add_entry_if_needed(project, version):
         pass
 
     # Debian versions must start with a digit
-    version = transform_version(latest_version)
+    version = transform_version(version)
 
     if version_changelog == version:
         return