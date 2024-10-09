osmocom/osmo-ci

commit 072b1eae88a89bba4585b38ef0e47ccc6c60bea9

manuals: publish pyosmocom manuals for releases
Adjust the building and publishing commands to work for pyosmocom.
Remove the 0.0.3 release from TAGS_IGNORE, because it already has
manuals.

Skip "apt-get -y build-dep" and adjusting "debian/control" if that file
does not exist, as it is the case with the 0.0.3 and 0.0.4 releases of
pyosmocom.

Change-Id: I3799cf825937cdd8dfca9fbd98e8b6b159f7cea8

scripts/manuals/publish-manuals-for-tags.sh
@@ -203,7 +203,6 @@ TAGS_IGNORE="
 
 	pyosmocom:0.0.1,
 	pyosmocom:0.0.2,
-	pyosmocom:0.0.3,
 "
 
 mkdir -p \
@@ -331,8 +330,10 @@ clone_repo() {
 	git clean -dxf
 
 	# Fix depends on packages that don't exist anymore
-	sed -i 's/dh-systemd \(.*\),//g' debian/control
-	sed -i 's/python-minimal,//g' debian/control
+	if [ -e debian/control ]; then
+		sed -i 's/dh-systemd \(.*\),//g' debian/control
+		sed -i 's/python-minimal,//g' debian/control
+	fi
 }
 
 # $1: repository
@@ -367,7 +368,9 @@ build_publish_manuals() {
 			# Install dependencies
 			case $repo in
 			*)
-				apt-get -y build-dep /build
+				if [ -e debian/control ]; then
+					apt-get -y build-dep /build
+				fi
 				;;
 			esac
 
@@ -386,6 +389,9 @@ build_publish_manuals() {
 			osmo-epdg)
 				su build -c \"make -C docs/manuals\"
 				;;
+			pyosmocom)
+				su build -c \"make -C docs html latexpdf\"
+				;;
 			*)
 				su build -c \"autoreconf -fi\"
 				su build -c \"./configure $configure_opts\"
@@ -403,6 +409,9 @@ build_publish_manuals() {
 				;;
 			osmo-epdg)
 				su build -c \"make -C docs/manuals publish\"
+				;;
+			pyosmocom)
+				su build -c \"make -C docs publish publish-html\"
 				;;
 			*)
 				su build -c \"make -C doc/manuals publish\"

commit 660177751752bd8db642bbfe7e03bce266feeddc

publish-manuals-for-tags: fix build deps install
Use the absolute path when checking for debian/control in
build_publish_manuals(), as we aren't in the /build directory at that
point.

Fixes: 072b1eae88 ("manuals: publish pyosmocom manuals for releases")
Change-Id: I8749107b974a4a8d57da868829a8f548404f3fa3

scripts/manuals/publish-manuals-for-tags.sh
@@ -368,7 +368,7 @@ build_publish_manuals() {
 			# Install dependencies
 			case $repo in
 			*)
-				if [ -e debian/control ]; then
+				if [ -e /build/debian/control ]; then
 					apt-get -y build-dep /build
 				fi
 				;;