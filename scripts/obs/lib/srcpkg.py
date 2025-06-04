osmocom/osmo-ci

commit 3b348d5effcd8b6cfde611b00473dd37545cebbb

OBS: remove pdf draft watermark for latest pkgs
Add the line
  ASCIIDOCSTYLE = $(BUILDDIR)\/custom-dblatex.sty
before the line
  include $(OSMO_GSM_MANUALS_DIR)/build/Makefile.asciidoc.inc
when building source packages for the latest feed, so there is no
"DRAFT" watermark.

Closes: OS#6188
Depends: osmo-gsm-manuals I33d67748b1e97578f4726009ac73fd0c4e7305db
Change-Id: Ie2e49ba89913321edaa90660a5348209355e61ce

scripts/obs/lib/srcpkg.py
@@ -167,6 +167,18 @@ def write_commit_txt(project):
     pathlib.Path(f"{output_path}/commit_{commit}.txt").touch()
 
 
+def set_asciidoc_style_without_draft_watermark(project):
+    repo_path = lib.git.get_repo_path(project)
+    doc_makefiles = lib.run_cmd(["grep", "-r", "-l", "include $(OSMO_GSM_MANUALS_DIR)/build/Makefile.asciidoc.inc"], cwd=repo_path)
+    doc_makefiles = doc_makefiles.output.rstrip().split("\n")
+
+    for doc_makefile in doc_makefiles:
+        print(f"{project}: setting asciidoc style to remove draft watermark in {doc_makefile}")
+        lib.run_cmd(["sed", "-i",
+                     '/\\/build\\/Makefile\\.asciidoc\\.inc/s/^/  ASCIIDOCSTYLE = $(BUILDDIR)\\/custom-dblatex.sty\\n/',
+                     doc_makefile], cwd=repo_path)
+
+
 def build(project, gerrit_id=0):
     conflict_version = lib.args.conflict_version
     feed = lib.args.feed
@@ -214,6 +226,8 @@ def build(project, gerrit_id=0):
 
     if lib.args.disable_manuals:
         lib.debian.disable_manuals(project)
+    elif feed == "latest":
+        set_asciidoc_style_without_draft_watermark(project)
 
     lib.debian.build_source_package(project)
     lib.debian.move_files_to_output(project)

commit 64b3a66d85930ca73ff777a964b7a9a05dc294f8

OBS: repo without manuals: fix draft removal logic
Adjust set_asciidoc_style_without_draft_watermark() so it does not fail
in repositories that do not have any manuals.

Fixes: 3b348d5e ("OBS: remove pdf draft watermark for latest pkgs")
Change-Id: Ie93430a1f7bd2bae6886e87755e79ff93ea609e7

scripts/obs/lib/srcpkg.py
@@ -169,10 +169,12 @@ def write_commit_txt(project):
 
 def set_asciidoc_style_without_draft_watermark(project):
     repo_path = lib.git.get_repo_path(project)
-    doc_makefiles = lib.run_cmd(["grep", "-r", "-l", "include $(OSMO_GSM_MANUALS_DIR)/build/Makefile.asciidoc.inc"], cwd=repo_path)
+    doc_makefiles = lib.run_cmd(["grep", "-r", "-l", "include $(OSMO_GSM_MANUALS_DIR)/build/Makefile.asciidoc.inc"], cwd=repo_path, check=False)
     doc_makefiles = doc_makefiles.output.rstrip().split("\n")
 
     for doc_makefile in doc_makefiles:
+        if doc_makefile == "":
+            continue
         print(f"{project}: setting asciidoc style to remove draft watermark in {doc_makefile}")
         lib.run_cmd(["sed", "-i",
                      '/\\/build\\/Makefile\\.asciidoc\\.inc/s/^/  ASCIIDOCSTYLE = $(BUILDDIR)\\/custom-dblatex.sty\\n/',