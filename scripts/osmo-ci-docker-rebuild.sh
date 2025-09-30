3891b72e6fab9189e8507c0fd4e2464402aa04cd
Oliver Smith <osmith@sysmocom.de>
2025-09-30 09:42:04 +0200

    scripts/osmo-ci-docker-rebuild: switch to trixie
    Depends: docker-playground I1c0efb85324b70b6370ea94f74f440e639442da5
    Change-Id: I1497f14e57f9d7b0867368b923c8f8919c72cd76

--- scripts/osmo-ci-docker-rebuild.sh
+++ scripts/osmo-ci-docker-rebuild.sh
@@ -5,16 +5,21 @@ cd "$(dirname "$0")/.."
 
 case "$(arch)" in
 x86_64)
+	# OS#6859: still need bookworm for osmo-opencm3-projects
 	docker_images_require \
 		"debian-bookworm-build" \
-		"debian-bookworm-erlang"
+		"debian-bookworm-erlang" \
+		"debian-trixie-build"
 	;;
 arm*|aarch64)
 	# OS#6627: need to run a separate "docker pull" command with platform
+	# OS#6858: still need bookworm for osmo-pcu
 	docker pull docker.io/arm32v7/debian:bookworm --platform="linux/arm/v7"
+	docker pull docker.io/arm32v7/debian:trixie --platform="linux/arm/v7"
 
 	export NO_DOCKER_IMAGE_PULL=1
 	docker_images_require \
-		"debian-bookworm-build-arm"
+		"debian-bookworm-build-arm" \
+		"debian-trixie-build-arm"
 	;;
 esac
