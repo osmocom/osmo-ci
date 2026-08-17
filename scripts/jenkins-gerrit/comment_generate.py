osmocom/osmo-ci

commit f30fab1cd276d8a9fd611e74f586c1f002543855

jenkins-gerrit/comment_generate: fail on 0 passed
Don't print the success message when zero jobs has passed. If this
happens, we have an internal error somewhere.

Change-Id: I1317f14c2f3f55f2a9b6581372c80edba33528db

scripts/jenkins-gerrit/comment_generate.py
@@ -212,6 +212,8 @@ def get_comment_result(build_url, notify_on_success):
 
     summary += f"{len(jobs['passed'])} passed:\n"
     summary += get_jobs_list_str(jobs["passed"])
+    if not jobs['passed']:
+        summary += "Zero jobs passed, internal script error?\n"
 
     if "build" in pipeline and "deb" in pipeline and "rpm" in pipeline and \
             not pipeline["build"]["passed"] and pipeline["deb"]["passed"] \
@@ -227,7 +229,7 @@ def get_comment_result(build_url, notify_on_success):
         summary += "https://osmocom.org/projects/cellular-infrastructure/wiki/Linting\n"
 
     summary += "\n"
-    if jobs["failed"]:
+    if jobs["failed"] or not jobs['passed']:
         summary += "Build Failed\n"
         summary += "\n"
         summary += f"Find the Retrigger button here:\n{build_url}\n"

commit 67988e370d00ff80868cbf8f7f7753289bfdf2db

jenkins-gerrit/comment_generate: fix 0 jobs msg
Check if there are no failed jobs as well before printing the "zero
jobs, internal script error?" message.

Fixes: f30fab1c ("jenkins-gerrit/comment_generate: fail on 0 passed")
Change-Id: I7289ce30492a84dedeac12bfc967d7c578f13790

scripts/jenkins-gerrit/comment_generate.py
@@ -212,8 +212,8 @@ def get_comment_result(build_url, notify_on_success):
 
     summary += f"{len(jobs['passed'])} passed:\n"
     summary += get_jobs_list_str(jobs["passed"])
-    if not jobs['passed']:
-        summary += "Zero jobs passed, internal script error?\n"
+    if not jobs["passed"] and not jobs["failed"]:
+        summary += "Zero jobs passed or failed, internal script error?\n"
 
     if "build" in pipeline and "deb" in pipeline and "rpm" in pipeline and \
             not pipeline["build"]["passed"] and pipeline["deb"]["passed"] \